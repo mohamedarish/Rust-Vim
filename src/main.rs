566bbba56d2335be8283633f46a1d925593d45e8
Mohamed Arish <Arish1248@gmail.com>
2022-12-01 22:08:03 +0530

    Figured out more neat tricks in termion and learnt what ncurses does and how termion does it

--- src/main.rs
+++ src/main.rs
@@ -6,10 +6,13 @@
 )]
 
 use std::io::Write;
+use termion::input::TermRead;
 
-use termion::{event::Key, input::TermRead};
+use termion::event::Key;
 
 fn main() {
+    println!("move up\nmove right\nmove left\nmove down");
+
     loop {
         let pressed_key = read_key();
 
@@ -17,8 +20,17 @@ fn main() {
             Ok(key) => match key {
                 Key::Ctrl('a') => break,
                 // Key::Char(c) => println!("{c}"),
-                Key::Char('\n') => println!("{}", termion::clear::All),
-                _ => println!("{key:?} {}", termion::clear::CurrentLine),
+                // Key::Char('\n') => println!("{}", termion::clear::All),
+                Key::Ctrl('f') => println!("{}", termion::color::Fg(termion::color::Red)),
+                Key::Ctrl('e') => println!("{}", termion::color::Bg(termion::color::White)),
+                Key::Ctrl('p') => println!("{}", termion::color::Bg(termion::color::Reset)),
+                Key::Ctrl('g') => println!("{}", termion::color::Fg(termion::color::Reset)),
+                Key::Char('\n') => println!("New Line"),
+                Key::Down => println!("{}", termion::cursor::Down(1)),
+                Key::Left => println!("{}", termion::cursor::Left(1)),
+                Key::Right => println!("{}", termion::cursor::Right(1)),
+                Key::Up => println!("{}", termion::cursor::Up(1)),
+                _ => println!("po"),
             },
             Err(error) => panic!("{}", error),
         }

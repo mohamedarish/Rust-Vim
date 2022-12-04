3282742be90152bebd84e4913aa65391ae638111
Mohamed Arish <Arish1248@gmail.com>
2022-12-04 14:02:27 +0530

    Made major code improvements

--- src/main.rs
+++ src/main.rs
@@ -9,35 +9,28 @@ use termion::raw::IntoRawMode;
 fn main() {
     println!("move up\nmove right\nmove left\nmove down");
 
-    let stdout = std::io::stdout().into_raw_mode().unwrap();
-    loop {
-        let pressed_key = read_key();
+    let stdin = std::io::stdin();
+    let mut stdout = std::io::stdout().into_raw_mode().unwrap();
 
-        match pressed_key {
-            Ok(key) => match key {
-                Key::Ctrl('a') => break,
-                // Key::Char(c) => println!("{c}"),
-                // Key::Char('\n') => println!("{}", termion::clear::All),
-                Key::Ctrl('f') => println!("{}", termion::color::Fg(termion::color::Red)),
-                Key::Ctrl('e') => println!("{}", termion::color::Bg(termion::color::White)),
-                Key::Ctrl('p') => println!("{}", termion::color::Bg(termion::color::Reset)),
-                Key::Ctrl('g') => println!("{}", termion::color::Fg(termion::color::Reset)),
-                Key::Char('\n') => println!("New Line"),
-                Key::Down => println!("{}", termion::cursor::Down(1)),
-                Key::Left => println!("{}", termion::cursor::Left(1)),
-                Key::Right => println!("{}", termion::cursor::Right(1)),
-                Key::Up => println!("{}", termion::cursor::Up(1)),
-                _ => println!("po"),
-            },
-            Err(error) => panic!("{error}"),
-        }
-    }
-}
+    write!(
+        stdout,
+        "{} {} Press :q to exit {}",
+        termion::clear::All,
+        termion::cursor::Goto(1, 1),
+        termion::cursor::Hide,
+    )
+    .unwrap();
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = std::io::stdin().lock().keys().next() {
-            return key;
+    stdout.flush().unwrap();
+
+    for key in stdin.keys() {
+        match key.unwrap() {
+            Key::Ctrl('a') => break,
+            Key::Char(c) => println!("{c}\r"),
+            _ => panic!("Error happened"),
         }
+        stdout.flush().unwrap();
     }
+
+    write!(stdout, "{} {}", termion::clear::All, termion::cursor::Show).unwrap();
 }
